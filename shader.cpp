dca40bafa273c8e5501e6ce15f0c0b45ea0736b1
mhoff12358 <mhoff12358@gmail.com>
2014-02-25 23:46:18 -0600

    Shader class now added in, but not really working

--- shader.cpp
+++ shader.cpp
@@ -1,9 +1,8 @@
 #include "shader.h"
 
-string& ShaderProgram::load_shader_file(const string& file_name) {
+int ShaderProgram::load_shader_file(const string& file_name, string& file_contents) {
 	ifstream src_file;
-	src_file.open(file_name.c_str());
-	string file_contents;
+	src_file.open(file_name);
 
 	if (src_file.is_open()) {
 		//The file successfully opened, load the file's contents
@@ -14,18 +13,58 @@ string& ShaderProgram::load_shader_file(const string& file_name) {
 		src_file.close();
 	} else {
 		//The file could not be opened report an error.
-		return NULL;
+		return 1;
 	}
-	std::cout << file_contents << std::endl;
-	return NULL;
+	return 0;
 }
 
-void ShaderProgram::go() {
-	load_shader_file("shader.cpp");
+void ShaderProgram::create_program() {
+	program = glCreateProgram();
 }
 
-int main() {
-	ShaderProgram sp;
-	sp.go();
+int ShaderProgram::add_shader(const GLenum shader_type, const string& file_name) {
+	GLuint shader = glCreateShader(shader_type);
+
+	//Load in the source code from the file
+	string shader_source;
+	if (!load_shader_file(file_name, shader_source)) return 1;
+
+	//Set the source and compile the shader
+	const char * src = shader_source.data();
+	glShaderSource(shader, 1, (const GLchar**)&src, NULL);
+	glCompileShader(shader);
+	GLint compile_success;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &compile_success);
+	if (compile_success == GL_FALSE) {
+		//Check for error specifics
+		GLint error_len = 0;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &error_len);
+
+		string error_msg;
+		error_msg.reserve(error_len);
+		glGetShaderInfoLog(shader, error_len, &error_len, (GLchar*)error_msg.data());
+
+		//Log the error (currently just print)
+		std::cout << error_msg << std::endl;
+
+		glDeleteShader(shader);
+		return 2;
+	}
+
+	//Attach the shader to the program
+	glAttachShader(program, shader);
+
+	//Add the shader to the list of included shaders
+	shaders.push_back(pair<GLuint, GLenum>(shader, shader_type));
+	std::cout << "SHADER SUCCESSFULLY ADDED" << std::endl;
+	return 0;
+}
+
+void ShaderProgram::link_program() {
+	glLinkProgram(program);
+}
+
+GLint ShaderProgram::get_program() {
+	// return program;
 	return 0;
 }
--- shader.h
+++ shader.h
@@ -1,17 +1,36 @@
 
+#ifndef __SHADER_H_INCLUDED__
+#define __SHADER_H_INCLUDED__
+
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <vector>
+#include <utility>
+
+#include <GL/glew.h>
 
 using std::string;
 using std::ifstream;
+using std::vector;
+using std::pair;
 
 class ShaderProgram {
 private:
 	// GLuint vert_shad, frag_shad, tess_shad, geo_shad;	
-	char * load_shader_file(const string&);
+	int load_shader_file(const string&, string&);
+
+	GLuint program;
+	vector<pair<GLuint, GLenum>> shaders;
 
 public:
-	void go();
+	int add_shader(const GLenum, const string&);
 
+	void create_program();
+
+	void link_program();
+
+	GLint get_program();
 };
+
+#endif
